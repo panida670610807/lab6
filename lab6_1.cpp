8eefbda127cf162e5ffd9dd6ba08817f08dacd69
panida670610807 <hiyik757@gmail.com>
2024-11-29 16:46:25 +0700

    finish lab6.1

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,34 @@
 using namespace std;
 
 int main(){
+    int i = 0, count =0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
-    return 0;
-}
+    cin>> i;
+     int count2 = 0;
+     while(i !=0){
+
+
+         if(i%2 == 0){
+             count = count+1;
+         }
+
+         if(i%2 !=0){
+            count2 = count2+1;}
+            
+            cout<<"Enter an integer: ";
+            cin>> i;
+         
+         }
+         
+         
+         
+     cout << "#Even numbers = " << count << "\n";
+     cout << "#Odd numbers = " << count2;
+     return 0;
+     }
+         
+         
+     
+
+    
+
